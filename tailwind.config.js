c7463a4b2d0c7dda3342e9a11493669925aadb27
Davidrami12 <davidrami2000@gmail.com>
2023-06-10 13:37:09 +0200

    install tailwind and setting up configurations

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#f5f5f5",
+        secondary: "#00f6ff"
+      }
+    },
+  },
+  plugins: [
+
+  ],
+}
+
