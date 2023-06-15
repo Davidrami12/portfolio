f3146dd8bd0d78c6bc449185fafe11f8570ecac2
Davidrami12 <davidrami2000@gmail.com>
2023-06-15 11:20:48 +0200

    Restructure assets folder and fixed tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  content: ["./public/**/*.{html,js}"],
+  content: ['./index.html', "./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         primary: "#f5f5f5",
         secondary: "#0078d4",
-        third: "005ba1"
       },
       fontFamily: {
-        /*'custom': ['Roboto', 'sans-serif']*/
         'custom': ['Roboto Slab', 'serif'],
       },
     },
